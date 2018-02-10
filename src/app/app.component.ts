90ac05f205fa9021c6ad3507f8f6e21e87ee2505
palinkas <p.patrik25@gmail.com>
2018-02-10 11:34:42 +0100

    chore: initial commit from @angular/cli
    (cherry picked from commit 958e9588e9e4eebe186eda966369646b5c980007)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  tomb = [1, 2, 3, 4, 5, 6, 7];
+
+  constructor() {
+   // console.log(this.tomb.reduce((x, y) => x + y));
+    const alma = this.tomb.filter((param: number) => param === 2);
+    console.log(alma);
+    console.log(this.tomb);
+  }
 }
